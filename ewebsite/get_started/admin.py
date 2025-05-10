3fb61163a0c0b53780e9c5481bbd06b3c0115c69
LepharamRamchiary <lepharamchiary@gmail.com>
2025-05-10 12:20:58 +0530

    displying database fields in admin panel into table wise

--- ewebsite/get_started/admin.py
+++ ewebsite/get_started/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Feed
 
 # Register your models here.
-admin.site.register(Feed)
+# admin.site.register(Feed)
+
+
+# displying database fields in admin panel into table wise
+@admin.register(Feed)
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'image', 'description', 'like_count', 'comment_count', 'share_count', 'created_at')
