ea46af87c3431bd6f6f971703d92f8b358830998
LepharamRamchiary <lepharamchiary@gmail.com>
2025-05-10 12:56:13 +0530

    Enhanced the feedback section with dynamic feed rendering.

--- ewebsite/get_started/views.py
+++ ewebsite/get_started/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Feed
 
 # Create your views here.
 def get_started(request):
     return render(request, 'get_started/get_started.html')
 
 def feed(request):
-    return render(request, 'get_started/feed.html')
+    feeds = Feed.objects.all()
+    return render(request, 'get_started/feed.html', {'feeds': feeds})
